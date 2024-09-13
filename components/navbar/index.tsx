9780d2ef925d2c14c9bafa1ea25e12bed2030157
Robin <kal6529@gmail.com>
2024-09-13 21:48:54 +0900

    [ADD] google 로그인 구현 및 로그인 상태에 따른 버튼 내용 및 기능 분기처리 - 로그인 시, log-out 버튼과 log-out 기능을 가진 버튼으로 작동 - 로그인 상태 아닐 시, log-in 버튼과 log-in 기능을 가진 버튼으로 작동

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,7 +1,34 @@
+"use client";
+
 import Link from "next/link";
 import { UserAvatar } from "./user-avatar";
+import { getUser } from "@/api/get/user";
+import { useEffect, useState } from "react";
+import { UserResponse } from "@supabase/supabase-js";
+import { UserButton } from "./user-button";
 
 export const Navbar = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [userData, setUserData] = useState<UserResponse>();
+
+  useEffect(() => {
+    const setInitUserData = async () => {
+      const userData = await getUser();
+
+      if (!userData.data.user) {
+        setIsLogin(false);
+      } else {
+        setIsLogin(true);
+        setUserData(userData);
+      }
+    };
+    setInitUserData();
+  }, []);
+
+  useEffect(() => {
+    console.log(userData);
+  }, [userData]);
+
   return (
     <nav className="py-5 px-2 border-b border-black flex justify-between items-center sticky top-0 z-50 bg-inherit">
       <div className="flex space-x-8 items-center">
@@ -14,7 +41,7 @@ export const Navbar = () => {
       <div className="flex space-x-5 items-center">
         {/* 아바타 버튼 (비로그인 시 회원가입 버튼) */}
         <UserAvatar />
-        <div>login</div>
+        <UserButton isLogin={isLogin} setIsLogin={setIsLogin} />
       </div>
     </nav>
   );
---
+++ components/navbar/user-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { googleLogin, logOut } from "@/api/get/user";
+import { redirect, useRouter } from "next/navigation";
+import { Dispatch, SetStateAction } from "react";
+
+interface UserButtonProps {
+  isLogin: boolean;
+  setIsLogin: Dispatch<SetStateAction<boolean>>;
+}
+
+export const UserButton = ({ isLogin, setIsLogin }: UserButtonProps) => {
+  const router = useRouter();
+
+  // toast나 modal로 에러처리
+  const onClickButton = async () => {
+    if (isLogin) {
+      // 로그인 되어 있을 때
+      const { error } = await logOut();
+
+      if (!error) {
+        router.push("/");
+        setIsLogin(false);
+      }
+    } else {
+      // 로그인 안되어 있을 때
+      await googleLogin();
+    }
+  };
+
+  return (
+    <button onClick={onClickButton}>{isLogin ? "log-out" : "log-in"}</button>
+  );
+};
